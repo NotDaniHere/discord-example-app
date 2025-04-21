528e610457e30e392e98198175cb1fbcc456c5c9
DaniHere <=>
2025-04-22 00:57:53 +0300

    my branch

--- app.js
+++ app.js
@@ -42,12 +42,14 @@ app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async fun
 
     // "test" command
     if (name === 'test') {
+      const randomVal = Math.random();
+      const faceCoin = randomVal < 0.5 ? 'Heads' : 'Tails';
       // Send a message into the channel where command was triggered from
       return res.send({
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
           // Fetches a random emoji to send from a helper function
-          content: `hello world ${getRandomEmoji()}`,
+          content: `Result: ${faceCoin}`,
         },
       });
     }
